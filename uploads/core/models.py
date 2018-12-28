35d0f11bf4d21499ad60fc9bbc008f1ad7004085
christinalee98 <j623lee@edu.uwaterloo.ca>
2018-12-28 02:06:27 -0500

    multiple file uploads!

--- uploads/core/models.py
+++ uploads/core/models.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django import forms
 
 class Document(models.Model):
 	title = models.CharField(max_length=30, blank=True)
 	description = models.CharField(max_length=255, blank=True)
-	document = models.FileField(upload_to='documents/')
+	file_1 = models.FileField(upload_to='documents/', blank=True)
+	file_2 = models.FileField(upload_to='documents/', blank=True)
+	file_3 = models.FileField(upload_to='documents/', blank=True)
+	file_4 = models.FileField(upload_to='documents/', blank=True)
+	file_5 = models.FileField(upload_to='documents/', blank=True)
 	uploaded_at = models.DateTimeField(auto_now_add=True)
     # User automatically collected
